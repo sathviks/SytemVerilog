8b01dbb8037d652e33c61d0c449c4dfaf85f9f18
sathvik.s <ssathvik1998@gmail.com>
2023-05-28 17:34:53 +0530

    data types and array 1 added

--- Fundamental_Procedural_Constructs/queues.sv
+++ Fundamental_Procedural_Constructs/queues.sv
@@ -1,26 +1,50 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 28.05.2023 17:15:02
-// Design Name: 
-// Module Name: queues
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+/*
+
+Size of this type shrink and grow automatically at run time 
+
+no need to define the size before simulation 
+
+
+this can be accesssed from any loction directly using index it
+
+
+queues >>> array in operations  
+
+
+it is effeciently utilizing memory comp to array
+
+*/
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module queues(
+module queues; 
 
-    );
+int arr[$]; // this is how declare queus
+
+
+initial begin 
+arr = { 1,2,3};
+arr.push_front(7);
+
+$display("display : %0p", arr);
+arr.push_back(0);
+
+$display("display : %0p", arr);           
+
+arr.insert(0,9);
+
+$display("display : %0p", arr);           
+    
+arr.delete(0);//0 is index
+$display("display : %0p", arr);           
+
+arr.pop_back();// return type is int 
+$display("display : %0p", arr);           
+
+end
+
+
+
 endmodule
