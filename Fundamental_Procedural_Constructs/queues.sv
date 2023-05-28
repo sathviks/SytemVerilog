e06e668d852aec11b11162ddc2a4016eaf3dbb97
sathvik.s <ssathvik1998@gmail.com>
2023-05-28 17:34:04 +0530

    data types and array 1 added

---
+++ Fundamental_Procedural_Constructs/queues.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 28.05.2023 17:15:02
+// Design Name: 
+// Module Name: queues
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module queues(
+
+    );
+endmodule
