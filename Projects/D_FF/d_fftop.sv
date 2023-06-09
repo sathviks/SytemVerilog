bdd25bdfd305c39bb837f0c82999c90af6c3d196
sathvik.s <ssathvik1998@gmail.com>
2023-06-09 18:37:33 +0530

    d_ff project added

---
+++ Projects/D_FF/d_fftop.sv
@@ -0,0 +1,27 @@
+
+
+module dff (dff_if vif);
+  
+  always@(posedge vif.clk)
+    begin
+      if(vif.rst == 1'b1)
+        vif.dout <= 1'b0;
+      else
+         vif.dout <= vif.din;
+    end
+  
+endmodule
+ 
+ 
+interface dff_if;
+  logic clk;
+  logic rst;
+  logic din;
+  logic dout;
+  
+endinterface
+
+
+
+
+
